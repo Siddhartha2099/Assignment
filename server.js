9bb8e1a6bbd1d0ef09bad7914c0f9f2869c1f5bc
siddhartha kanoujia <siddharthakanoujia20@gmail.com>
2020-04-19 13:50:21 +0530

    message

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+//app.set('view engine', 'hbs');
+const port=process.env.PORT || 3000;
+hbs.registerPartials(__dirname + '/views/partial')
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+});
+hbs.registerHelper('Capt',(text)=>{
+  return text.toUpperCase();
+});
+hbs.registerHelper('qwe',([string=''])=>{
+  return string.toLowerFirst();
+});
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeMessage:'Welcome to my Assignment'
+  });
+});
+app.get('/Agriculture',(req,res)=>{
+  res.render('Agriculture.hbs',{
+      pageTitle:'Agriculture Types',
+  }
+  );
+});
+app.get('/pgdetails',(req,res)=>{
+  res.render('pgdetails.hbs',{
+      pageTitle:'Agriculture types details',
+  }
+  )
+})
+app.get('/bad', (req, res) => {
+  res.send({
+  errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server is up on port $ {port}');
+});
+
